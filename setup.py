549c1abc198e93c213bf09a3de25fbd5d5684528
Kailash Sharma <61358755+kailash1011@users.noreply.github.com>
2020-06-28 20:59:18 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name = "temppack",
+      packages = ['temppack'],
+      download_url = "https://github.com/kailash1011/temppack/archive/v_0.1.tar.gz",
+      version = '1.0',
+      description = "Complete module to convert units of temperature.",
+      url = "https://github.com/kailash1011/temppack",
+      author = "Kailash Sharma",
+      author_email = "kailashps.1011@gmail.com",
+      license = "MIT",
+      classifiers = ['Programming Language :: Python :: 3.6'],
+
+
+      )
