50d2920c0442c07491ae59c762aadf02815ba3f8
Kailash Sharma <61358755+kailash1011@users.noreply.github.com>
2020-06-28 21:19:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup,find_packages
-
+with open('Readme.txt') as file:
+    long_descriptionx = file.read()
 setup(name = "temppack",
       packages = find_packages(),
       download_url = "https://github.com/kailash1011/temppack/archive/v_0.1.tar.gz",
@@ -10,6 +11,6 @@ setup(name = "temppack",
       author_email = "kailashps.1011@gmail.com",
       license = "MIT",
       classifiers = ['Programming Language :: Python :: 3.6'],
-
+      long_description = long_descriptionx
 
       )
